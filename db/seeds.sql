a711983f5a1605042170db1de960e0864d6cc43a
Your Name <hp2393@gmail.com>
2019-05-07 10:08:50 -0400

    added seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO BURGERS (burger_name, devoured) VALUES ("Green-Chile Burgers", FALSE);
+INSERT INTO BURGERS (burger_name, devoured) VALUES ("Cheddar-and-Onion Smashed Burgers", FALSE);
+INSERT INTO BURGERS (burger_name, devoured) VALUES ("Bacon Burger", FALSE);
